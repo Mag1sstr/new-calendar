53a6f19418740b7d7b5aed8b6c956518015d3765
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-03-01 02:07:22 +0500

    Corrected the design

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,18 +3,33 @@ import styles from "./calendar.module.css";
 const weekDays = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
 
 export default function Calendar() {
+  const currentDate = new Date();
+  let date = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
   return (
     <div className={styles.calendar}>
       <h1 className={styles.month}>Январь</h1>
       <div className={styles.row}>
         <div className={styles.info}></div>
         <div className={styles.wrapper}>
-          {weekDays.map((day) => (
-            <div className={styles.week__day}>{day}</div>
-          ))}
-          {[...Array(35)].map((_, i) => {
-            return <div key={i} className={styles.days}></div>;
-          })}
+          <div className={styles.week}>
+            {weekDays.map((day) => (
+              <div className={styles.week__day}>{day}</div>
+            ))}
+          </div>
+          <div className={styles.month__days}>
+            {[...Array(date.getDate())].map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className={`${styles.days} ${
+                    currentDate.getDate() === i + 1 && styles.current
+                  }`}
+                >
+                  <p className={styles.numeric}>{i + 1}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
